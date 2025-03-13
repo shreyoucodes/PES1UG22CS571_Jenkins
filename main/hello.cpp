f2467ceb34657898920c78d17ca6efd7917fceba
shreyoucodes <shreyadallas14@gmail.com>
2025-03-13 19:44:14 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "PES1UG22CS571 is smart \n";
+  cout << "made a change \n";
   return 0;
 }
